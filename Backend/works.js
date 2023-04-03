4a2c50ecf3e0bd01cbdd284084c591a76e93d6ca
Lucthir <lucas.thiron@gmail.com>
2023-04-03 17:54:09 +0200

    Mise en page

--- Backend/works.js
+++ Backend/works.js
@@ -1,3 +1,5 @@
+//Recuperation Données API
+
 let works = window.localStorage.getItem('works');
 
 if (works === null) {
@@ -16,15 +18,18 @@ function generateWorks(works) {
     for (let i=0; i < works.length; i++) {
 
         const article = works[i];
+        // Récupération de l'élément du DOM qui accueillera les elements gallery
         const sectionGallery = document.querySelector(".gallery");
+        // Création d’une balise dédiée à un travail
         const workElement = document.createElement("article");
         workElement.dataset.id = works[i].id;
+        // Création des balises titres et images
         const imageElement = document.createElement("img");
         imageElement.src = article.imageUrl;
         const titleElement = document.createElement("h3");
         titleElement.innerText = article.title;
 
-
+        // On rattache la balise article a la section Gallery
         sectionGallery.appendChild(workElement);
         workElement.appendChild(imageElement);
         workElement.appendChild(titleElement);
@@ -32,3 +37,5 @@ function generateWorks(works) {
 }
 
 generateWorks(works);
+
+
